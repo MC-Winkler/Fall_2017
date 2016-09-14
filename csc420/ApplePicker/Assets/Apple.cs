fb86686df2f37078b3d03b92349b71f87f5ffcbb
Michael Winkler <mwinkler3@mooney202-15.elon.edu>
2016-09-14 17:15:40 -0400

    add apple picker

---
+++ csc420/ApplePicker/Assets/Apple.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Apple : MonoBehaviour {
+
+	private float screenBottom = -20f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.transform.position.y <= screenBottom) {
+			Destroy (this.gameObject);
+		}
+	}
+}
---
+++ csc420/ApplePicker/Assets/AppleTree.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppleTree : MonoBehaviour {
+
+	public GameObject applePrefab;
+	public float speed = 12.4f;
+	public float leftAndRightEdge = 16f;
+	public float chanceToChangeDirections = .03f;
+	public float secondsBetweenAppleDrops = 1.2f;
+
+	private void DropApple() {
+		GameObject appleObject = Instantiate (applePrefab) as GameObject;
+		appleObject.transform.position = transform.position;
+	}
+
+	void FixedUpdate() {
+		float directionCheck = Random.value;
+		if (directionCheck < chanceToChangeDirections){
+			speed = -speed;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		if (pos.x >= leftAndRightEdge) {
+			speed = -Mathf.Abs(speed);
+		}
+		if (pos.x <= -leftAndRightEdge) {
+			speed = Mathf.Abs (speed);
+		}
+	}
+}
