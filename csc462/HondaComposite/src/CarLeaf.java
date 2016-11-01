7bbfa8c46894bb45d1acdfe9d23de50382f9c1c5
Michael Winkler <mwinkler3@carpenter-12.elon.edu>
2016-11-01 12:21:32 -0400

    almost finish hondaComposite

--- csc462/HondaComposite/src/CarLeaf.java
+++ csc462/HondaComposite/src/CarLeaf.java
@@ -2,45 +2,28 @@ import java.util.Iterator;
 
 public class CarLeaf extends CarComponent {
 	String name;
-	String description;
-	boolean isFree;
 	double price;
-	
-	public CarLeaf (String name,
-			String description,
-			boolean isFree,
-			double price) {
+
+	public CarLeaf(String name, double price) {
 		this.name = name;
-		this.description = description;
-		this.isFree = isFree;
 		this.price = price;
 	}
-	
-	public String getName(){
+
+	public String getName() {
 		return name;
 	}
-	
-	public String getDescription() {
-		return description;
-	}
-	
+
 	public double getPrice() {
 		return price;
 	}
-	
-	public boolean isFree() {
-		return isFree;
-	}
-	
+
 	public void print() {
 		System.out.print(getName());
-		if (!isFree){
-			System.out.print("\t\t\t");
-			System.out.print("$" + getPrice());
-		}
+		System.out.print("\t\t\t");
+		System.out.print("$" + getPrice());
 		System.out.println();
 	}
-	
+
 	public Iterator<CarComponent> createIterator() {
 		return new NullIterator();
 	}
