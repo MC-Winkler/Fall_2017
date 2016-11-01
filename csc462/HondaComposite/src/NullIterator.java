439d10dad0c9d6784291484d256acd884ab2c5fc
Michael <mwinkler3@elon.edu>
2016-11-01 00:33:38 -0400

    add csc462 hondaComposite start

---
+++ csc462/HondaComposite/src/NullIterator.java
@@ -0,0 +1,19 @@
+import java.util.Iterator;
+
+public class NullIterator implements Iterator<CarComponent>{
+
+	@Override
+	public boolean hasNext() {
+		return false;
+	}
+
+	@Override
+	public CarComponent next() {
+		return null;
+	}
+
+	public void remove() {
+		throw new UnsupportedOperationException();
+	}
+	
+}
